e9763b392a884337ae733e59edd7be62f7b64958
David Kanjuru Wahome <dave-cmd@debian.debian.com>
2020-09-13 00:35:05 +0300

    content cards javascript update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ const navbar_links =  document.getElementsByClassName('navbar-links')[0]
 toggle_menu.addEventListener('click', ()=>{
     navbar_links.classList.toggle('active')
 })
+
+// Main Content
+const main_content =  document.getElementsByClassName('main-content')[0]
+console.log(main_content.children)
+
+
+for (let i=0; i<main_content.children.length; i++){
+    main_content.children[i].addEventListener('click', ()=>{
+        console.log('click', i)
+        const card = document.getElementsByClassName('card')[i]
+        card.classList.toggle('active')
+        
+    })
+}
